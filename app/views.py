5315aaa11bba1e19859d6dacfeded77f3479592e
Cezar Azevedo de Faveri <cazevedodefaveri@gmail.com>
2017-04-30 10:38:38 -0300

    Added more trees and improved style

--- app/views.py
+++ app/views.py
@@ -4,25 +4,23 @@ from flask import render_template
 import pandas as pd
 
 data = pd.read_csv('arbolado-publico-lineal.csv', delimiter=';')
-print(data.shape)
+
+data['LAT'] = pd.to_numeric(data['LAT'].str.replace(',', '.'))
+data['LONG'] = pd.to_numeric(data['LONG'].str.replace(',', '.'))
 
 polen_trees = ['PLATANUS']
 
 polen_data = data.loc[data['NOMBRE_GEN'].isin(polen_trees)]
 no_polen_data = data.loc[~data['NOMBRE_GEN'].isin(polen_trees)]
 
-print (polen_data.shape)
-print (no_polen_data.shape)
-
-polen_data['LAT'] = pd.to_numeric(polen_data['LAT'].str.replace(',', '.'))
-polen_data['LONG'] = pd.to_numeric(polen_data['LONG'].str.replace(',', '.'))
 polen_data_coords = polen_data[['LAT', 'LONG']]
 
-print(polen_data_coords.shape)
-# polen_data_coords = polen_data_coords[1:2000]
+citrus_lemon = no_polen_data[no_polen_data['NOMBRE_CIE'] == "CITRUS LIMON"][['LAT', 'LONG']]
+mora_data = no_polen_data[no_polen_data['NOMBRE_CIE'] == 'MORUS ALBA'][['LAT', 'LONG']]
+orange_data = no_polen_data[no_polen_data['NOMBRE_CIE'] == 'CITRUS SINENSIS'][['LAT', 'LONG']]
 
+coord_data = [polen_data_coords, citrus_lemon, mora_data, orange_data]
 @app.route('/')
 def index():
-    return render_template('index.html', polen_data_coords=polen_data_coords)
+    return render_template('index.html', data=coord_data)
 
-app.run(debug=True)
